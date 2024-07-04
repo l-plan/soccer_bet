899dcde579bb66456e1279f872deac28c021d8e6
rolf <rolf@l-plan.nl>
2024-07-04 16:45:51 +0200

    renamed xls-printxls

--- app/lib/xls/print_xls.rb
+++ app/lib/xls/print_xls.rb
@@ -1,30 +1,31 @@
+module Xls
+	module PrintXls
+		require 'spreadsheet'
 
-module PrintXls
-	require 'spreadsheet'
+
+		def save_to_stream
+
+			io = StringIO.new
+			xls.write io
+			io.rewind
+
+			io
+		end
+
+		def date_format
+			Spreadsheet::Format.new :number_format => 'DD/MM/YYYY'
+		end
+
+		def time_format
+			Spreadsheet::Format.new :number_format => 'hh:mm'
+		end
+
+		def number_format
+			Spreadsheet::Format.new :number_format => "#,##0.00"
+		end
+
+		
 
 
-	def save_to_stream
-
-		io = StringIO.new
-		xls.write io
-		io.rewind
-
-		io
 	end
-
-	def date_format
-		Spreadsheet::Format.new :number_format => 'DD/MM/YYYY'
-	end
-
-	def time_format
-		Spreadsheet::Format.new :number_format => 'hh:mm'
-	end
-
-	def number_format
-		Spreadsheet::Format.new :number_format => "#,##0.00"
-	end
-
-	
-
-
 end
